93e45b54cd548b562a311366cdb0703b245dd5f3
akolade <adesanmiakoladedotun@gmail.com>
2019-01-21 06:35:11 +0100

    chore(setup): integrate ci and cd

---
+++ client/setups/setup.js
@@ -0,0 +1,7 @@
+/* eslint-disable */
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+// import renderer from 'react-test-renderer';
+
+configure({ adapter: new Adapter() });
+
