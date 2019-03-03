ba4f5825cd2a3c0d42a25b576dc58fdcf0a68f89
akolliy1 <adesanmiakoladedotun@gmail.com>
2019-03-03 02:07:10 +0100

    refactor(redux): add z-index to feed Navbar and fix carousel proptypes error

--- src/features/feed/common/IntroSection.jsx
+++ src/features/feed/common/IntroSection.jsx
@@ -22,7 +22,9 @@ const IntroSection = ({ push }) => (
     className="feed-intro-section img-fluid intro-section-fixed-height"
     style={{ backgroundImage: `url(${bgImage})` }}
   >
-    <PrimaryNavbar push={push} bukka />
+    <div className="intro-navbar-area">
+      <PrimaryNavbar push={push} bukka />
+    </div>
     <div className="fixed-top text-left" style={{ zIndex: '100' }}>
       <LargeTextSection />
     </div>
