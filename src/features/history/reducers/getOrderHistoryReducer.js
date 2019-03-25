2e25a1260ef032b720362acf99b01f1555670d01
akolliy1 <adesanmiakoladedotun@gmail.com>
2019-03-25 01:31:15 +0100

    feature(redux): integrate redux and fetched history data

---
+++ src/features/history/reducers/getOrderHistoryReducer.js
@@ -0,0 +1,42 @@
+import { FETCH_USER_ORDER_HISTORY, } from 'Redux/actionTypes';
+
+const initialState = {
+  status: {
+    fetched: false,
+    error: false,
+  },
+  orderHistory: '',
+  errorMessage: ''
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case `${FETCH_USER_ORDER_HISTORY}_SUCCESS`:
+      return {
+        ...state,
+        status: {
+          ...state.status,
+          fetched: true,
+          error: false,
+        },
+        orderHistory: action.data,
+        errorMessage: ''
+      };
+
+    case `${FETCH_USER_ORDER_HISTORY}_ERROR`:
+      return {
+        ...state,
+        status: {
+          ...state.status,
+          fetched: true,
+          error: true
+        },
+        orderHistory: {},
+        errorMessage: action.data
+      };
+
+    default: {
+      return state;
+    }
+  }
+};
