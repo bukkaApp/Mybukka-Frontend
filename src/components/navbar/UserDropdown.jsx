9c0429949b2be14349ffd0dcfbdc95bccfd63d57
Akolade <adesanmiakoladedotun@gmail.com>
2019-05-01 03:30:33 +0100

    bug(fix): fix bukka delivery price tag base on delivery mode

--- src/components/navbar/UserDropdown.jsx
+++ src/components/navbar/UserDropdown.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+
 import signOut from './actionCreators/signOut';
+
 import './userdropdown.scss';
 
 const UserDropdown = ({ logoutUser }) => {
