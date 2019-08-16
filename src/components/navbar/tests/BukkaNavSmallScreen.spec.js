e2b6ab671470a7820be2aac3695ad4aedd39c7dc
Akolade <adesanmiakoladedotun@gmail.com>
2019-08-16 08:38:26 +0100

    fix(cleanup): bukka page

--- src/components/navbar/tests/BukkaNavSmallScreen.spec.js
+++ src/components/navbar/tests/BukkaNavSmallScreen.spec.js
@@ -3,7 +3,12 @@ import React from 'react';
 import BukkaNavSmallScreen from '../BukkaNavSmallScreen';
 
 describe('Bukka Nav Small Screen', () => {
-  const wrapper = mount(<BukkaNavSmallScreen currentCategory="fish" />);
+  const wrapper = mount(
+    <BukkaNavSmallScreen
+      categoryItems={['fish', 'egg']}
+      currentCategory="fish"
+    />
+  );
 
   it('renders properly', () => {
     expect(wrapper).toMatchSnapshot();
