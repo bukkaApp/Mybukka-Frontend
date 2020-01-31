a37bcab8a68a5af53a0c07401edb2aa20aa3dc92
akolade <adesanmiakoladedotun@gmail.com>
2020-01-31 15:48:55 +0100

    fix heroku integration

--- src/features/modal-root/Index.jsx
+++ src/features/modal-root/Index.jsx
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types';
 import AuthModal from 'Components/navbar/common/AuthModal';
 import InviteFriends from '../invite/InviteFriends';
 
-const Index = ({ push }) => (
+export const Index = ({ push }) => (
   <>
     <AuthModal push={push} />
     <InviteFriends push={push} />
