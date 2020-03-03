ca4cc32d0d90dbd1593f847d8580c14cca966cf0
akolade <adesanmiakoladedotun@gmail.com>
2020-03-03 22:38:03 +0100

    Refactor(DB): change to staging database

--- src/redux/axios/index.js
+++ src/redux/axios/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const { NODE_ENV } = process.env;
 const PORT = process.env.PORT || '1234';
-const baseURL = NODE_ENV === 'production' ? 'https://backendapi.mybukka.com/api/v1/' : `http://localhost:${PORT}/api/v1/`; // eslint-disable-line
+const baseURL = NODE_ENV === 'production' ? 'https://mybukka-backend.herokuapp.com/api/v1/' : `http://localhost:${PORT}/api/v1/`; // eslint-disable-line
 
 const instance = axios.create({
   baseURL,
