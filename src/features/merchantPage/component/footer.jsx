5192dade5e80e1119ba98884203b50e59ea2049f
ufas-001 <ufasabdulmutolib@gmail.com>
2020-04-20 17:12:02 +0100

    refactored the page for small screen

--- src/features/merchantPage/component/footer.jsx
+++ src/features/merchantPage/component/footer.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import './footer.css';
 import Logo from '../../../../src/components/brand/Logo';
+import '../index.scss'
 const Footer = () => (
-    <div class="footer pt-4 pb-4">
-        <div class="container">
-            <div class="row txt-white">
-                <div class="bukka-description col-md-4 f-16 height-19">
+    <div className="footer pt-4 pb-4">
+        <div className="container">
+            <div className="row txt-white">
+                <div className="bukka-description col-md-4 f-16 height-19">
                     <Logo />
-                    <div class="pt-5 txt-white">
+                    <div className="pt-5 txt-white f-16">
                         <p>The largest, most reliable on-demand delivery and pickup platform.</p>
-                        <p class="pt-2">Able to deliver anything from any where, MyBukka is the food delivery, grocery delivery, whatever you can think of delivery app to bring what you crave right to your door.</p>
+                        <p className="pt-2">Able to deliver anything from any where, MyBukka is the food delivery, grocery delivery, whatever you can think of delivery app to bring what you crave right to your door.</p>
                     </div>
                 </div>
-                <div class="bukka-platforms col-md-2 f-15 height-18">
+                <div className="bukka-platforms col-md-2 f-15 height-18">
                     <h5>BUKKA</h5>
                     <p>About</p>
                     <p>Blog</p>
@@ -21,17 +22,17 @@ const Footer = () => (
                     <p>Promtion Offers</p>
                     <p>Pickups</p>
                 </div>
-                <div class="col-md-2 f-15 height-18">
+                <div className="col-md-2 f-15 height-18">
                     <h5>MERCHANTS</h5>
                     <p>Sell on Bukka</p>
                     <p>Restaurants</p>
-                    <div class="pt-5">
+                    <div className="pt-5">
                         <h4>SUPPORTS</h4>
                         <p>Customer Help</p>
                         <p>Merchant Help</p>
                     </div>
                 </div>
-                <div class="bukka-cities col-md-2 f-15 height-18">
+                <div className="bukka-cities col-md-2 f-15 height-18">
                     <h5>CITIES</h5>
                     <p>Lagos</p>
                     <p>Akure</p>
@@ -40,7 +41,7 @@ const Footer = () => (
                     <p>Kogi</p>
                     <p>Kano</p>
                 </div>
-                <div class="bukka-handle col-md-2 f-15 height-18">
+                <div className="bukka-handle col-md-2 f-15 height-18">
                     <h5>FOLLOW US</h5>
                     <p>Twitter</p>
                     <p>Facebook</p>
@@ -48,12 +49,12 @@ const Footer = () => (
                     <p>LinkedIn</p>
                 </div>
             </div>
-            <hr class="line" />
-            <div class="d-flex justify-content txt-white f-16">
+            <hr className="line" />
+            <div className="d-privacy justify-content txt-white f-16">
                 <p>©2019 MYBUKKA INC TERMS PRIVACY </p>
                 <div>
-                    <span class="pr-4">Terms and Policy</span>
-                    <span>Privacy</span>
+                    <span className="pr-4 policy">Terms and Policy</span>
+                    <span className="privacy">Privacy</span>
                 </div>
             </div>
 
