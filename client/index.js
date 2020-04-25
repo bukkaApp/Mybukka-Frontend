ba0aec5f59e376289356b13522b5c4402f1dcbbc
Akolade <adesanmiakoladedotun@gmail.com>
2020-04-26 00:27:18 +0100

    upgrade app from legacy to concurrent mode

--- client/index.js
+++ client/index.js
@@ -1,5 +1,24 @@
-import React from 'react';
-import { render } from 'react-dom';
+import React, { StrictMode } from 'react';
+// import { render } from 'react-dom';
+import runtime from 'serviceworker-webpack-plugin/lib/runtime';
+import { createRoot } from 'react-dom';
 import App from '../src/root/App';
+// import * as serviceWorker from './serviceWorker';
 
-render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById('root');
+
+createRoot(rootElement).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
+/* eslint-disable no-unused-vars */
+
+if ('serviceWorker' in navigator) {
+  const registration = runtime.register();
+}
