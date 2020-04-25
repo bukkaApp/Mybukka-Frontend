38ec9e33d64212a342d617790d7b2619eb947f30
Akolade <adesanmiakoladedotun@gmail.com>
2020-04-26 00:01:18 +0100

    enhance app with react lazy and suspense

--- src/root/App.jsx
+++ src/root/App.jsx
@@ -1,14 +1,12 @@
 import 'babel-polyfill'; // eslint-disable-line
 
-import React from 'react';
+import React, { Suspense } from 'react';
 
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import { toast } from 'react-toastify';
 
 import { PersistGate } from 'redux-persist/integration/react';
 
-// import AuthModal from 'Components/navbar/common/AuthModal';
 import reduxStore from '../redux/store';
 import Main from './Main';
 import IndeterminateProgressbar from
@@ -17,22 +15,24 @@ import AlertMessage from '../components/alert';
 
 import './index.scss';
 import './animate.scss';
-
-toast.configure();
+import ContextProviders from './ContextProviders';
 
 const { store, persistor } = reduxStore();
 
 const App = () => (
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <IndeterminateProgressbar />
-      <AlertMessage />
-      {/* <AuthModal push={() => {}} /> */}
-      <BrowserRouter>
-        <Main />
-      </BrowserRouter>
-    </PersistGate>
-  </Provider>
+  <Suspense fallback={<div>Loading...</div>}>
+    <BrowserRouter>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <IndeterminateProgressbar />
+          <AlertMessage />
+          <ContextProviders>
+            <Main />
+          </ContextProviders>
+        </PersistGate>
+      </Provider>
+    </BrowserRouter>
+  </Suspense>
 );
 
 export default App;
