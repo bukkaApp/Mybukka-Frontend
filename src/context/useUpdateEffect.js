b55592ee2b5e21863d0eaaeab27a5d9d6fadde19
Akolade <adesanmiakoladedotun@gmail.com>
2020-04-26 02:04:38 +0100

    improve useUpdateEffect hook

--- src/context/useUpdateEffect.js
+++ src/context/useUpdateEffect.js
@@ -1,15 +1,25 @@
 import { useRef, useEffect } from 'react';
+import useIsMounted from './useIsMounted';
 
 const useUpdateEffect = (effect, dependencies = []) => {
+  const isMounted = useIsMounted();
   const isInitialMount = useRef(true);
 
   useEffect(() => {
+    let effectCleanupFunc = function noop() {};
     if (isInitialMount.current) {
       isInitialMount.current = false;
     } else {
-      effect();
+      effectCleanupFunc = effect() || effectCleanupFunc;
     }
+    return () => {
+      effectCleanupFunc();
+      if (!isMounted.current) {
+        isInitialMount.current = true;
+      }
+    };
   }, dependencies);
 };
 
 export default useUpdateEffect;
+
