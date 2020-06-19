df0fefb410b1c7dfa3771e609ed1c04cb8e605cf
Akolade <adesanmiakoladedotun@gmail.com>
2020-06-19 08:26:44 +0100

    Setup css polyfill for older browser support

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,12 @@
 require('dotenv').config();// CommonsChunkPlugin
 
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const ServiceWorkerWebpackPlugin = require('serviceworker-webpack-plugin');
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-/* Import copy-webpack-plugin */
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const CompressionPlugin = require('compression-webpack-plugin');
-const BrotliPlugin = require('brotli-webpack-plugin');
-const MinimizerPlugin = require('./webpack.minimizer');
+const ServiceWorkerWebpackPlugin = require('serviceworker-webpack-plugin');
 
-const CopyPlugin = new CopyWebpackPlugin({
-  patterns: [
-    {
-      // path to the folder to be copied
-      from: path.resolve(__dirname, 'client/pwa'),
-    },
-  ],
-});
+const MinimizerPlugin = require('./webpack.minimizer');
 
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
   template: './client/index.html',
@@ -72,56 +59,17 @@ module.exports = {
     filename: '[name].[hash].js',
     publicPath: '/',
   },
-  optimization: {
-    splitChunks: {
-      chunks: 'all',
-    },
-    minimize: true,
-    minimizer: [
-      MinimizerPlugin.minifyJavaScript(),
-      MinimizerPlugin.minifyCss()
-    ]
-  },
+  optimization: MinimizerPlugin.optimization(),
   plugins: [
-    new webpack.HashedModuleIdsPlugin({
-      context: __dirname,
-      hashFunction: 'sha256',
-      hashDigest: 'hex',
-      hashDigestLength: 20
-    }),
-    new CompressionPlugin({
-      filename: '[path].gz[query]',
-      algorithm: 'gzip',
-      test: /\.js$|\.css$|\.html$/,
-      threshold: 10240,
-      minRatio: 0.8,
-      deleteOriginalAssets: false,
-    }),
-    new BrotliPlugin({
-      filename: '[path].br[query]',
-      algorithm: 'brotliCompress',
-      test: /\.(js|css|html|svg)$/,
-      compressionOptions: {
-        level: 11,
-      },
-      threshold: 10240,
-      minRatio: 0.7,
-      deleteOriginalAssets: false,
-    }),
-    new CleanWebpackPlugin({ dry: true, }),
+    ...(process.env.NODE_ENV === 'production' ? MinimizerPlugin.plugins() : []),
     MiniCssPlugin,
     HtmlWebpackPluginConfig,
     defineVariablesPlugin,
     ServiceWorkerPlugin,
-    CopyPlugin,
   ],
   module: {
     rules: [
-      {
-        test: /\.gz$/,
-        enforce: 'pre',
-        use: 'gzip-loader'
-      },
+      ...(process.env.NODE_ENV === 'production' ? MinimizerPlugin.rules() : []),
       {
         test: /\.jsx?$/,
         loader: 'babel-loader',
@@ -135,6 +83,7 @@ module.exports = {
       },
       {
         test: /\.(sa|sc|c)ss$/,
+        exclude: /node_modules/,
         use: [
           {
             loader: MiniCssExtractPlugin.loader,
@@ -143,6 +92,7 @@ module.exports = {
             },
           },
           'css-loader',
+          'postcss-loader',
           'sass-loader',
         ],
       },
--- webpack.minimizer.js
+++ webpack.minimizer.js
@@ -1,34 +1,54 @@
 /* eslint-disable import/no-extraneous-dependencies */
+// https://webpack.js.org/configuration/optimization/
+// https://webpack.js.org/guides/tree-shaking/
+// https://github.com/webpack-contrib/terser-webpack-plugin#terseroptions
 const TerserPlugin = require('terser-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const safePostCssParser = require('postcss-safe-parser');
+// const autoprefixer = require('autoprefixer');
 
-const shouldUseSourceMap = process.env.GENERATE_SOURCEMAP !== 'false';
+require('dotenv').config();// CommonsChunkPlugin
+
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require('path');
+const webpack = require('webpack');
+/* Import copy-webpack-plugin */
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
+const BrotliPlugin = require('brotli-webpack-plugin');
 
 
-exports.minifyJavaScript = () =>
+const shouldUseSourceMap = process.env.NODE_ENV === 'production';
+
+
+// path to the folder to be copied
+const CopyPlugin = new CopyWebpackPlugin({
+  patterns: [{ from: path.resolve(__dirname, 'client/pwa'), }],
+});
+
+
+// https://survivejs.com/webpack/styling/autoprefixing/
+// exports.autoprefixPlugin = () => new webpack.LoaderOptionsPlugin({
+//   options: {
+//     postcss: [autoprefixer()]
+//   }
+// });
+
+
+const minifyJavaScript = () =>
   new TerserPlugin({
     cache: true,
     parallel: true,
-    sourceMap: true, // Must be set to true if using source-maps in production
+    sourceMap: shouldUseSourceMap, // Must be set to true if using source-maps in production
     terserOptions: {
       // https://github.com/webpack-contrib/terser-webpack-plugin#terseroptions,
       ecma: undefined,
       warnings: false,
       parse: { ecma: 8 },
-      compress: {
-        ecma: 5,
-        warnings: false,
-        comparisons: false,
-        inline: 2
-      },
+      compress: { ecma: 5, warnings: false, comparisons: false, inline: 2 },
       mangle: true, // Note `mangle.properties` is `false` by default.
       module: false,
-      output: {
-        ecma: 5,
-        comments: false,
-        ascii_only: true
-      },
+      output: { ecma: 5, comments: false, ascii_only: true },
       toplevel: false,
       nameCache: null,
       ie8: false,
@@ -38,14 +58,64 @@ exports.minifyJavaScript = () =>
     }
   });
 
-exports.minifyCss = () => new OptimizeCSSAssetsPlugin({
+
+const minifyCss = () => new OptimizeCSSAssetsPlugin({
   cssProcessorOptions: {
     parser: safePostCssParser,
-    map: shouldUseSourceMap
-      ? {
-        inline: false,
-        annotation: true
-      }
-      : false
+    map: shouldUseSourceMap ? { inline: false, annotation: true } : false
   }
 });
+
+
+exports.optimization = () => ({
+  splitChunks: {
+    // chunks: 'all',
+    chunks: 'async',
+    minSize: 30000,
+    // minRemainingSize: 0,
+    maxSize: 0,
+    minChunks: 1,
+    maxAsyncRequests: 6,
+    maxInitialRequests: 4,
+    automaticNameDelimiter: '~',
+    cacheGroups: {
+      defaultVendors: { test: /[\\/]node_modules[\\/]/, priority: -10 },
+      default: { minChunks: 2, priority: -20, reuseExistingChunk: true }
+    }
+  },
+  minimize: true,
+  minimizer: [minifyJavaScript(), minifyCss()]
+});
+
+
+exports.plugins = () => ([
+  new webpack.HashedModuleIdsPlugin({
+    context: __dirname,
+    hashFunction: 'sha256',
+    hashDigest: 'hex',
+    hashDigestLength: 20
+  }),
+  new CompressionPlugin({
+    filename: '[path].gz[query]',
+    algorithm: 'gzip',
+    test: /\.js$|\.css$|\.html$/,
+    threshold: 10240,
+    minRatio: 0.8,
+    deleteOriginalAssets: true,
+  }),
+  new BrotliPlugin({
+    filename: '[path].br[query]',
+    algorithm: 'brotliCompress',
+    test: /\.(js|css|html|svg)$/,
+    compressionOptions: { level: 11 },
+    threshold: 10240,
+    minRatio: 0.7,
+    deleteOriginalAssets: true,
+  }),
+  new CleanWebpackPlugin({ dry: true }),
+  CopyPlugin,
+]);
+
+exports.rules = () => ([
+  { test: /\.gz$/, enforce: 'pre', use: 'gzip-loader' },
+]);
